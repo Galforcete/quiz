3a511ef02f616a9df97ab3b277c4b14e190b9e3d
Galforcete <miguelangel.munoz.sanz@alumnos.upm.es>
2015-05-07 20:08:50 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,11 @@ router.get('/', function(req, res) {
 
 /* GET author page. */
 router.get('/author', function(req, res) {
-  res.render('author', { title: 'Quiz' });
+  res.render('author', { title: 'Autor' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
