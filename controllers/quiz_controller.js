fec298944a3e5c26d8c0a3050b9a1ea2015275ed
Galforcete <miguelangel.munoz.sanz@alumnos.upm.es>
2015-05-11 08:57:08 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,23 @@
 var models = require('../models/models.js');
 
+// GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build( // Crea objeto Quiz
+				{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res){
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})  // Redirección HTTP (URL relativo) lista de preguntas
+};
+
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
 	models.Quiz.find(quizId).then(
