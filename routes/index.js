96fde280005ec19a6660d0b75bbf14eff3f87dc5
Galforcete <miguelangel.munoz.sanz@alumnos.upm.es>
2015-05-18 16:54:21 +0200

    Estadísticas

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var dbController = require('../controllers/db_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
@@ -33,6 +34,7 @@ router.post('/quizes/create', sessionController.loginRequired, quizController.cr
 router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
 router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
+router.get('/quizes/statistics', dbController.calculate);
 
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
